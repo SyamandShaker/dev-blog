c666a73deca9cd4ac3e5f2d447796bf108c6e11c
SyamandShaker <syamand.shaker@powercoders.org>
2020-04-21 10:33:32 +0200

    add hamberger menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
+
+const body = document.querySelector("body");
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+
+openMenu.addEventListener('click', function() {
+  body.classList.add('menu-is-open');
+});
+closeMenu.addEventListener('click', function() {
+  body.classList.remove('menu-is-open');
+});
+/** 
 $(document).ready(function() {
 $(function(){
     $('.hemberger').click(function(){
@@ -5,3 +17,4 @@ $(function(){
     })
  });
 });
+*/
