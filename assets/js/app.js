d62840ee44ecc2c330c402a21153fd08a699c154
SyamandShaker <syamand.shaker@powercoders.org>
2020-05-15 01:03:15 +0200

    update all posts and add circle right and left

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,31 @@
-$(window).scroll(function() {    
-  let scroll = $(window).scrollTop();
+$(document).ready(function(){
 
-  if (scroll >= 35) {
-      $(".clearHeader").addClass("darkHeader");
-  } else {
-      $(".clearHeader").removeClass("darkHeader");
-  }
+  /* $(window).scroll(function() {    
+    let scroll = $(window).scrollTop();
+  
+    if (scroll >= 35) {
+        $(".clearHeader").addClass("darkHeader");
+    } else {
+        $(".clearHeader").removeClass("darkHeader");
+    }
+  }); */
+  
+    var lastScrollTop = 0;
+
+    $(window).scroll(function(event){
+
+      var st = $(this).scrollTop();
+
+      if (st >= 96) {
+
+        if (st > lastScrollTop){
+          $(".clearHeader").removeClass("darkHeader");
+        } else {
+          $(".clearHeader").addClass("darkHeader");
+        }
+        lastScrollTop = st; //console.log(lastScrollTop);
+
+      }
+    });
+
 });
